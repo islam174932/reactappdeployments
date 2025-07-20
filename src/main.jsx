385c7c466df29f0878a2b04633f40702def1192a
islam174932 <eslamgamal19933@gmail.com>
2025-07-20 20:04:42 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "bootstrap/dist/css/bootstrap.min.css";
 // Remove this line: import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
